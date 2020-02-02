fe54271a95e4b430ae650097c53618bd18c2ce92
Nauval Rizky <enuma.alrizky@gmail.com>
2020-02-03 01:53:35 +0900

    andromeda: parts: raise2wake: check proximity before wakeup
    * to avoid having unnecessary wakeups when phone inside the pocket
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: I19075420686e54945eda1f1b726d95b41a30471a
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- parts/src/org/lineageos/settings/doze/PickupSensor.java
+++ parts/src/org/lineageos/settings/doze/PickupSensor.java
@@ -43,12 +43,18 @@ public class PickupSensor implements SensorEventListener {
     private Context mContext;
     private ExecutorService mExecutorService;
 
+    private Sensor mProximitySensor;
+    private boolean mInsidePocket = false;
+
     private long mEntryTimestamp;
 
     public PickupSensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = DozeUtils.getSensor(mSensorManager, "xiaomi.sensor.pickup");
+        mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
@@ -67,8 +73,18 @@ public class PickupSensor implements SensorEventListener {
 
         mEntryTimestamp = SystemClock.elapsedRealtime();
 
-        if (event.values[0] == 1) {
-            DozeUtils.launchDozePulse(mContext);
+        if (!isRaiseToWake && !DozeUtils.isPocketGestureEnabled(mContext)) {
+            mInsidePocket = false;
+        }
+
+        if (event.values[0] == 1 && !mInsidePocket) {
+            if (isRaiseToWake) {
+                mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
+                mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                    PowerManager.WAKE_REASON_GESTURE, TAG);
+            } else {
+                DozeUtils.launchDozePulse(mContext);
+            }
         }
     }
 
@@ -77,19 +93,37 @@ public class PickupSensor implements SensorEventListener {
         /* Empty */
     }
 
+    private SensorEventListener mProximityListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            mInsidePocket = event.values[0] < mProximitySensor.getMaximumRange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // stub
+        }
+    };
+
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
         submit(() -> {
-            mSensorManager.registerListener(this, mSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            if (DozeUtils.isRaiseToWakeEnabled(mContext)) {
+                mSensorManager.registerListener(mProximityListener, mProximitySensor,
+                        SensorManager.SENSOR_DELAY_NORMAL);
+            }
             mEntryTimestamp = SystemClock.elapsedRealtime();
         });
     }
 
     protected void disable() {
-        if (DEBUG) Log.d(TAG, "Disabling");
-        submit(() -> {
-            mSensorManager.unregisterListener(this, mSensor);
+        if (DEBUG)
+            Log.d(TAG, "Disabling");
+        submit(() -> { mSensorManager.unregisterListener(this, mSensor);
+            if (DozeUtils.isRaiseToWakeEnabled(mContext)) {
+                mSensorManager.unregisterListener(mProximityListener, mProximitySensor);
+            }
         });
     }
 }
