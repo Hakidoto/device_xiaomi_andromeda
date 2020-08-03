d2c624a4795a3a319b1502fcccc128fb36db0795
Nicholas Magill <nmagill1979@gmail.com>
2020-08-03 16:27:55 +0100

    andromeda: Add Google Camera
    MGC_7.4.104_Parrot043-v1.apk

--- device.mk
+++ device.mk
@@ -104,7 +104,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gcam/system/etc/permissions/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
