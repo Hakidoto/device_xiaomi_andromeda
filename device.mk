f65ac220c1f8faa2c44a33095f956a76fbd523ad
Nicholas Magill <nmagill1979@gmail.com>
2020-11-26 00:05:13 +0000

    andromeda: Update graphics stack from LA.UM.9.1.r1-07300-SMxxx0.0

--- device.mk
+++ device.mk
@@ -131,12 +131,16 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.2-service \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.composer@2.4-service \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.mapper@3.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0
 
 PRODUCT_PACKAGES += \
     gralloc.msmnile \
